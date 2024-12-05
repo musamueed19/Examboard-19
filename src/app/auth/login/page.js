23bc1ef31c98365b406c16e3201a7488b2ff8f56
Muhammad Musa Mueed <musavuswh@gmail.com>
2024-12-05 22:30:17 +0500

    fix stlying for login page, part-2

--- src/app/auth/login/page.js
+++ src/app/auth/login/page.js
@@ -3,7 +3,7 @@ import LoginForm from "@/components/Forms/LoginForm";
 function LoginPage() {
   return (
     <div className="w-full h-screen bg-[#eeeeee] flex items-center justify-center">
-      <div className="md:max-w-md w-full h-screen md:h-fit p-8 bg-white rounded-md flex flex-col gap-8 items-center justify-center drop-shadow-xl">
+      <div className="md:max-w-md w-full h-screen md:h-fit px-8 bg-white rounded-md flex flex-col space-y-8 items-center justify-center drop-shadow-xl">
         <h1 className="w-full text-[#226FFE] font-bold text-3xl text-center">Sign in</h1>
 
         <LoginForm />
